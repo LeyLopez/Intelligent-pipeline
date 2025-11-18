e0368bfc58976744753d237dd0b6222f3fb449f2
LeyLopez <leidislopez2017@gmail.com>
2025-11-17 21:06:41 -0500

    Dependecies and Sklearn-Model configuration

---
+++ sklearn_model/app/config.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    service_name: str = "sklearn-model"
+    version: str = "1.0.0"
+
+    api_host: str = "0.0.0.0"
+    api_port: int = 8002
+
+    mlflow_tracking_uri: str = "http://localhost:5000"
+    mlflow_experiment_name: str = "sklearn_classification"
+
+
+    model_path: str = "./models/classifier.joblib"
+    model_type: str = "classification"
+
+    log_level: str = "INFO"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
+
+
